892955b40976d4cce48c5bb2fd4f7dd057736bd2
Eliossul <ltalleceven@gmail.com>
2023-11-06 15:20:17 +0100

    Update hash.php

--- hash.php
+++ hash.php
@@ -2,11 +2,11 @@
 
 $connexion = new PDO("mysql:host=127.0.0.1;dbname=gsbV2;port=3306", "root", "password");
 
-$requeteMdp = "SELECT MDP FROM visiteur ORDER BY MDP";
+$requeteMdp = "SELECT mdp FROM visiteur ORDER BY mdp";
 $resultatsMdp = $connexion->query($requeteMdp);
 $arrayMdp[] =  $resultatsMdp->fetchAll();
 
-$requeteId= "SELECT MATRICULE FROM visiteur ORDER BY MDP";
+$requeteId= "SELECT id FROM visiteur ORDER BY mdp";
 $resultatsId = $connexion->query($requeteId);
 $arrayId[] =  $resultatsId->fetchAll();
 
@@ -15,7 +15,7 @@ for ($i = 0; $i < count($arrayMdp); $i++)
     for($j = 0; $j < count($arrayMdp[0]); $j++)
     {
         $mdpActuel = md5($arrayMdp[$i][$j][0]);
-        $sth = $connexion->prepare('UPDATE visiteur SET MDP = ? WHERE MATRICULE = ? ');
+        $sth = $connexion->prepare('UPDATE visiteur SET mdp = ? WHERE id = ? ');
         $sth->execute([$mdpActuel,$arrayId[$i][$j][0]]);
         echo($mdpActuel);
         echo(" ");
